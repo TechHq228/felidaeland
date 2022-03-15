7d3cd1445a206aac906a6211104b5bd78a593c04
TechHq228 <egor11ero@gmail.com>
2022-03-15 07:42:50 +0000

    v1.2
    input validation...

--- public/script.js
+++ public/script.js
@@ -15,18 +15,45 @@ var button = document.querySelector(".button3")
 
 var names = document.querySelectorAll("#catNick")
 
+var duties = ['Catizen', 'Purrliceman', 'Purrfessor in Catford', 'Fake Opposition Member', 'Claws of Justice Unit', 'Catlomat Delegate', 'Catnip Supply Minister', 'Sunbathing Spots Minister', 'Teacher at Kittygarten']
+
+var selected = document.querySelector("select")
+
+var banned = ['Dovahdein', 'SirCoffee17', 'Ceska_Z']
+
+//setInterval(valid, 1000)
+
+function valid() {
+	if (duties.indexOf(selected.value) > -1) return
+	else {
+		button.remove()
+		alert("No.")
+		document.location.reload()
+	}
+}
+
 function time() {
  hid.value = current
+	if (hid.value != current) {
+		textInput.value = null
+                alert("Time travel isn't allowed.")
+                document.location.reload()
+	}
 }
-time()
+//time()
 
 button.addEventListener("click", () => {
+	time()
+	valid()
     for (let i = 0; i < names.length; i++) {
         var name = names[i].innerText
-        console.log(name)
         if (textInput.value == name) {
                     textInput.value = null
                     alert("You're already registered")
-                }
+        } else if (banned.indexOf(textInput.value) > -1) {
+		textInput.value = null
+		alert("Get outta here!")
+	} 
+	return
     }
 })
